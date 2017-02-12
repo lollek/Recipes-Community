d6faf1523d0e4cc0e310fffb080e1351dc6d3d3a
Olle Kvarnström <olle+git@iix.se>
2017-02-12 13:56:21 +0100

    Throw in some twitter bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'main-app',
+    template: `
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <a class="navbar-brand" href="#">Start</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Favouries</a>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'jquery';
+
 platformBrowserDynamic().bootstrapModule(AppModule);
