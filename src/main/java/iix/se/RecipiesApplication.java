ffea2b60881c6c271ecc2d5ab5894538e1f425c3
Olle Kvarnström <olle+git@iix.se>
2017-02-12 12:14:50 +0100

    Initialize project

---
+++ src/main/java/iix/se/RecipiesApplication.java
@@ -0,0 +1,12 @@
+package iix.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipiesApplication.class, args);
+	}
+}
