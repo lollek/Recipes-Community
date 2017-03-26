d1993fdb8a66280b3077aca9812caa98334cdf2a
Olle Kvarnström <olle+git@iix.se>
2017-03-26 16:51:31 +0200

    Rename package iix.se -> se.iix

--- src/main/java/se/iix/RecipiesApplication.java
+++ src/main/java/se/iix/RecipiesApplication.java
@@ -1,4 +1,4 @@
-package iix.se;
+package se.iix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class RecipiesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipiesApplication.class, args);
 	}
+
 }
