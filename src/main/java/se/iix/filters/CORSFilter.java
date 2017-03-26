699bf387b651e531597119f43e8a3d36fa0b9dd5
Olle Kvarnström <olle+git@iix.se>
2017-03-26 16:59:37 +0200

    Set CORS to allow any origin.
    If possible, I should restrict this later on, but I will probably not. It is probably worth to set it on a production server, but as this is just a simple course. It would be really bad if I set this now and then I mess up my presentation because of it being hosted on a different location.

---
+++ src/main/java/se/iix/filters/CORSFilter.java
@@ -0,0 +1,26 @@
+package se.iix.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+
+}
