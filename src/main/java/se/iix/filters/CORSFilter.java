5fe0dbb279886116d575b184ea8e8b2e2d0fc4a6
Olle Kvarnström <olle+git@iix.se>
2017-04-01 17:45:40 +0200

    Add extra CORS parameters which seem to be recommended

--- src/main/java/se/iix/filters/CORSFilter.java
+++ src/main/java/se/iix/filters/CORSFilter.java
@@ -11,9 +11,12 @@ public class CORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(req, res);
     }
 
