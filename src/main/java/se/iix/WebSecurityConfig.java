103b9d367ba09a6787b2b9de2109a7a385acc119
Olle Kvarnström <olle+git@iix.se>
2017-04-02 19:55:59 +0200

    Set authorization requirement for non-GET commands.
     User auth is still not working though.

--- src/main/java/se/iix/WebSecurityConfig.java
+++ src/main/java/se/iix/WebSecurityConfig.java
@@ -1,16 +1,34 @@
 package se.iix;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception
+    {
+        webSecurity
+                .ignoring()
+                .antMatchers("/api/auth/**")
+                .antMatchers("/h2-console/**");
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .anyRequest().authenticated();
+
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
