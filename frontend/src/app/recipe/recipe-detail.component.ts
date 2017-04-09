cea5d04faf856652b255c38bbffd949d09ea3fcc
Olle Kvarnström <olle+git@iix.se>
2017-04-09 17:35:21 +0200

    Made it possible to modify a recipe. Not yet possible to save though

--- frontend/src/app/recipe/recipe-detail.component.ts
+++ frontend/src/app/recipe/recipe-detail.component.ts
@@ -1,35 +1,97 @@
 import {Component} from "@angular/core";
-import {ActivatedRoute, Router, Params} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 import {RecipeService} from "./recipe.service";
 import {Recipe} from "./recipe.model";
 
 @Component({
     selector: 'recipe-detail',
     template: `
-<div>
-    <h2 class="text-center" [innerText]="recipe?.title"></h2>
+<div *ngIf="!isEditing">
+    <h2 class="text-center">
+        <span>{{ recipe?.title }}</span>
+        <button class="btn btn-info" (click)="onEdit()">Edit</button>
+    </h2>
     <h6 class="text-center">Created by {{ recipe?.author }}</h6>
+    <div class="mx-auto">
+    </div>
     <div [innerText]="recipe?.instructions">
     
     </div>
 </div>
+
+<form *ngIf="isEditing" #recipeForm="ngForm" (ngSubmit)="onSubmit()">
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text"
+               id="title"
+               class="form-control"
+               name="title"
+               [(ngModel)]="recipe.title"
+               #title="ngModel"
+               required>
+    </div>
+    <div *ngIf="title.errors && (title.dirty || title.touched)"
+         class="alert alert-danger">
+         <div [hidden]="!title.errors.required">
+            Title is required
+        </div>
+    </div>
+    
+    <div class="form-group">
+        <label for="instructions">Instructions</label>
+        <textarea id="instructions"
+                  class="form-control"
+                  name="instructions"
+                  [(ngModel)]="recipe.instructions"
+                  #instructions="ngModel"
+                  rows="10"
+                  required></textarea>
+    </div>
+    <div *ngIf="instructions.errors && (instructions.dirty || instructions.touched)"
+         class="alert alert-danger">
+         <div [hidden]="!instructions.errors.required">
+            Instructions are required
+        </div>
+    </div>
+    
+    <button class="btn btn-primary" type="submit" [disabled]="!recipeForm.form.valid">Save</button>
+    <button class="btn btn-warning" (click)="onCancel()">Cancel</button>
+</form>
+
+
 `
 })
 
 export class RecipeDetailComponent {
 
-    private recipe: Recipe;
+    recipe: Recipe;
+    isEditing: boolean = false;
 
     constructor(
         private route: ActivatedRoute,
-        private router: Router,
         private service: RecipeService
     ) {
     }
 
+    //noinspection JSUnusedGlobalSymbols
     ngOnInit() {
         this.route.params
             .switchMap((params: Params) => this.service.findById(+params['id']))
             .subscribe((recipe: Recipe) => this.recipe = recipe)
+    }
+
+    //noinspection JSUnusedGlobalSymbols
+    onEdit() {
+        this.isEditing = true;
+    }
+
+    //noinspection JSUnusedGlobalSymbols
+    onSubmit() {
+        this.isEditing = false;
+    }
+
+    //noinspection JSUnusedGlobalSymbols
+    onCancel() {
+        this.isEditing = false;
     }
 }
--- frontend/src/app/recipe/recipe.module.ts
+++ frontend/src/app/recipe/recipe.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from "@angular/core";
 import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+
 import {RecipeService} from "./recipe.service";
 import {RecipeListComponent} from "./recipe-list.component";
-import {BrowserModule} from "@angular/platform-browser";
 import {RecipeDetailComponent} from "./recipe-detail.component";
 import {RecipeRoutingModule} from "./recipe-routing.module";
-
 
 @NgModule({
     declarations: [ RecipeListComponent, RecipeDetailComponent ],
     providers: [ RecipeService ],
-    imports: [ BrowserModule, HttpModule, RecipeRoutingModule ],
-    exports: [ RecipeListComponent ]
+    imports: [ BrowserModule, HttpModule, FormsModule, RecipeRoutingModule ],
+    exports: [ ]
 })
 
 export class RecipeModule { }
