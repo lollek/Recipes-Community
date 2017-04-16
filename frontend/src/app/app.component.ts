92be07b2a6f736a22418053d290cb5b755d9de8a
Olle Kvarnström <olle+git@iix.se>
2017-04-16 17:17:10 +0200

    Redesign navbar.
    Change to Home | Cookbook | New Recipe |                  (search for recipe()) Login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,30 +1,47 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "./auth/auth.service";
 
 @Component({
     selector: 'main-app',
     template: `
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-toggleable navbar-light bg-faded">
     <a class="navbar-brand" routerLink="/">Home</a>
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-            <a class="nav-link" routerLink="/mypage" routerLinkActive="active">My page</a>
+            <a class="nav-link" routerLink="/cookbook" routerLinkActive="active">Cookbook</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" routerLink="/favorites" routerLinkActive="active">Favourites</a>
+            <a class="nav-link" routerLink="/newrecipe" routerLinkActive="active">New Recipe</a>
         </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2"
-               type="text"
-               placeholder="Search for recipes"
-               [(ngModel)]="searchQuery"
-               [ngModelOptions]="{standalone: true}">
-        <button class="btn btn-outline-primary my-2 my-sm-0"
-                type="submit"
-                (click)="doSearch()"
-                [disabled]="!searchQuery">Search</button>
+    <form class="form-inline">
+        <div class="input-group">
+            <input class="form-control"
+                   type="text"
+                   placeholder="Search for recipes"
+                   [(ngModel)]="searchQuery"
+                   [ngModelOptions]="{standalone: true}">
+            <div class="input-group-btn">
+                <button class="btn btn-default"
+                        type="submit"
+                        (click)="doSearch()"
+                        [disabled]="!searchQuery">
+                            <span class="fa fa-search"></span>
+                </button>
+            </div>
+        </div>
     </form>
+    <div>
+        <a *ngIf="!isLoggedIn" class="nav-link" routerLink="/login">
+            <span class="fa fa-sign-in"></span>
+            Login
+        </a>
+        <a *ngIf="isLoggedIn" class="nav-link" (click)="logout()">
+            <span class="fa fa-sign-out"></span>
+            Logout
+        </a>
+    </div>
 </nav>
 <div class="container pt-3">
     <router-outlet></router-outlet>
@@ -35,13 +52,22 @@ import {Router} from "@angular/router";
 export class AppComponent  {
     private searchQuery: string;
 
+    private get isLoggedIn(): boolean {
+        return this.authService.isLoggedIn;
+    }
+
     constructor(
-        private router: Router
+        private router: Router,
+        private authService: AuthService
     ) {
     }
 
     private doSearch(): void {
         //noinspection JSIgnoredPromiseFromCall
         this.router.navigate(['/search', this.searchQuery]);
+    }
+
+    private logout(): void {
+        this.authService.logout();
     }
 }
